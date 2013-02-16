01332dc0832f370cb607d3e76b7658f9e106f71a
Robin Bjorklin <robin.bjorklin@gmail.com>
2013-02-16 16:19:57 +0100

    Added scala syntax highlighting for vim and a basic setup for tmux.conf

--- setup-my-system.sh
+++ setup-my-system.sh
@@ -5,7 +5,7 @@ cd ~/
 echo "Installing Development Tools"
 sudo yum group install "Development Tools"
 echo "Installing git, tmux..."
-sudo yum install git tmux zsh vim-enhanced mosh levien-inconsolata-fonts xterm wget
+sudo yum install git tmux zsh vim-enhanced mosh levien-inconsolata-fonts xterm wget irssi
 
 echo "Setting up ZSH"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -17,16 +17,16 @@ chsh -s /bin/zsh
 echo "Setting up vim"
 cp /etc/vimrc ~/.vimrc
 
-echo "set background=dark" > ~/.vimrc
-echo "colors solarized" > ~/.vimrc
-echo "set tabstop=2" > ~/.vimrc
-echo "set expandtab" > ~/.vimrc
-echo "set shiftwidth=2" > ~/.vimrc
-echo "set number" > ~/.vimrc
+echo "set background=dark" >> ~/.vimrc
+echo "colors solarized" >> ~/.vimrc
+echo "set tabstop=2" >> ~/.vimrc
+echo "set expandtab" >> ~/.vimrc
+echo "set shiftwidth=2" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
 
 git clone git://github.com/altercation/solarized.git
 mkdir ~/.vim
-cp solarized/vim-colors-solarized/* ~/.vim
+mv solarized/vim-colors-solarized/* ~/.vim
 rm -rf solarized
 
 echo "Fetching latest hosts file from someonewhocares"
@@ -35,21 +35,38 @@ sudo mv hosts /etc/hosts
 
 echo "Fetching Solarized .Xresources"
 git clone git://github.com/solarized/xresources.git
-cp xresources/solarized ~/.Xresources
+mv xresources/solarized ~/.Xresources
 rm -rf xresources
 
-echo "xterm*.faceName: Inconsolata" > ~/.Xresources
-echo "xterm*.faceSize: 12" > ~/.Xresources
-echo "xterm*.geometry: 80x25" > ~/.Xresources
-echo "xterm*.scrollBar: false" > ~/.Xresources
-echo "Xft.dpi: 96" > ~/.Xresources
-echo "Xft.hintstyle: hintfull" > ~/.Xresources
-echo "Xft.hinting: true" > ~/.Xresources
+echo "xterm*.faceName: Inconsolata" >> ~/.Xresources
+echo "xterm*.faceSize: 12" >> ~/.Xresources
+echo "xterm*.geometry: 80x25" >> ~/.Xresources
+echo "xterm*.scrollBar: false" >> ~/.Xresources
+echo "Xft.dpi: 96" >> ~/.Xresources
+echo "Xft.hintstyle: hintfull" >> ~/.Xresources
+echo "Xft.hinting: true" >> ~/.Xresources
 
 echo "Fetching Solarized .dircolors"
 git clone git://github.com/seebi/dircolors-solarized.git
-cp dircolors-solarized/dircolors.256dark ~/.dircolors
+mv dircolors-solarized/dircolors.256dark ~/.dircolors
 rm -rf dircolors-solarized
+
+echo "Fetching scala syntax highlighting and indentation for vim"
+git clone https://github.com/scala/scala-dist.git
+mv ~/scala-dist/tool-support/src/vim/* ~/.vim 
+rm -rf ~/scala-dist
+
+echo "Setting vi binds for tmux"
+echo "set -g history-limit 50000                 " >> ~/.tmux.conf
+echo "setw -g mode-keys vi                       " >> ~/.tmux.conf
+echo "unbind [                                   " >> ~/.tmux.conf
+echo "unbind p                                   " >> ~/.tmux.conf
+echo "unbind Escape                              " >> ~/.tmux.conf
+echo "bind-key Escape copy-mode                  " >> ~/.tmux.conf
+echo "bind-key p paste-buffer                    " >> ~/.tmux.conf
+echo "bind-key -t vi-copy v begin-selection      " >> ~/.tmux.conf
+echo "bind-key -t vi-copy y copy-selection       " >> ~/.tmux.conf
+echo "bind-key C-c run 'tmux show-buffer | xclip'" >> ~/.tmux.conf
 
 echo "Zenbook UX31 color profile"
 echo "http://www.notebookcheck.net/uploads/tx_nbc2/UX31.icc"
