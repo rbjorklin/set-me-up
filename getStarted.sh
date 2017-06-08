2d5316842d2f08bb056bc8200e8c351fb85bc962
Robin Björklin <robin.bjorklin@apptus.com>
2017-06-08 11:49:59 +0200

    Abandoned st terminal after multiple copy-paste issues

--- getStarted.sh
+++ getStarted.sh
@@ -7,7 +7,7 @@ if [[ $# -ne 2 ]] ; then
 fi
 
 sudo mkdir -p /etc/salt/minion.d
-sudo cp /srv/files/local.conf /etc/salt/minion.d/
+sudo cp /srv/local.conf /etc/salt/minion.d/
 sudo dnf install -y salt-minion
 echo "Starting salt state, this might take a few minutes."
 sudo salt-call -l quiet state.highstate pillar="{'user':'$1','type':'$2'}"
