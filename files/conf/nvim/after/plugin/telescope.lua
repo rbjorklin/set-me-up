9b1c60b34f3979197397b49c342f454759a13bf3
Robin Björklin <robin.bjorklin@gmail.com>
2023-05-05 13:52:32 -0700

    feat(nvim): add more telescope keybindings
    Found here: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua

--- files/conf/nvim/after/plugin/telescope.lua
+++ files/conf/nvim/after/plugin/telescope.lua
@@ -4,3 +4,14 @@ if (not status) then return end
 local keymap = vim.keymap.set
 keymap('n', '<LEADER>f', builtin.find_files, {})
 keymap('n', '<LEADER>g', builtin.live_grep, {})
+keymap('n', '<LEADER>t?', builtin.oldfiles, { desc = '[t?] Find recently opened files'})
+keymap('n', '<LEADER>t<SPACE>', builtin.buffers, { desc = '[t ] Find existing buffers'})
+keymap('n', '<LEADER>t/', function()
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[t/] Fuzzily search in current buffer' })
+
+keymap('n', '<LEADER>tsh', require('telescope.builtin').help_tags, { desc = '[T]elescope [S]earch [H]elp' })
+keymap('n', '<LEADER>tk', require('telescope.builtin').keymaps, { desc = '[T]elescope [K]eymaps Help' })
