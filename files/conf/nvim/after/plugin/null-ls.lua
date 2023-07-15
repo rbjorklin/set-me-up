9c98681d245a819cc614253bb112d1fdd1e7e3d4
Robin Björklin <robin.bjorklin@gmail.com>
2023-07-14 21:14:23 -0700

    fix: don't auto-format python files

--- files/conf/nvim/after/plugin/null-ls.lua
+++ files/conf/nvim/after/plugin/null-ls.lua
@@ -8,7 +8,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.ocamlformat,
         --null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
+        --null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.hclfmt,
         --null_ls.builtins.formatting.jq,
         null_ls.builtins.formatting.packer,
