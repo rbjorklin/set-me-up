65bc229017c52e3faffdea74911d47284b965b3c
Robin Björklin <robin.bjorklin@gmail.com>
2024-04-28 16:41:03 -0700

    chore(nvim): cleanup null-ls formatting

--- files/conf/nvim/after/plugin/null-ls.lua
+++ files/conf/nvim/after/plugin/null-ls.lua
@@ -9,26 +9,15 @@ null_ls.setup({
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
-        null_ls.builtins.formatting.ocamlformat,
-        --null_ls.builtins.formatting.stylua,
-        --null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.ocamlformat, -- skip autoformat on save for a single write with ':noautocmd w' or ':noa w' for short.
         null_ls.builtins.formatting.hclfmt,
-        --null_ls.builtins.formatting.jq,
         null_ls.builtins.formatting.packer,
         null_ls.builtins.formatting.terraform_fmt,
-        --null_ls.builtins.formatting.yq,
-        --null_ls.builtins.formatting.sqlfluff.with({
-        --    extra_args = { "--dialect", "postgres" }, -- change to your dialect
-        --}),
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.completion.luasnip,
         null_ls.builtins.code_actions.gitsigns,
-        --null_ls.builtins.code_actions.shellcheck,
-        --null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.diagnostics.rstcheck,
         null_ls.builtins.diagnostics.yamllint,
     },
-    -- Formatting on save - https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+    -- Formatting on save - https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -36,7 +25,7 @@ null_ls.setup({
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
+										vim.lsp.buf.formatting_sync()
                 end,
             })
         end
