c8b1901c3acfbbdf02bf563b96c74dd30231d126
Robin Björklin <robin.bjorklin@gmail.com>
2026-03-09 11:33:39 -0700

    feat: run linter on file save

--- stow/dotfiles/dot-config/nvim/after/plugin/nvim-lint.lua
+++ stow/dotfiles/dot-config/nvim/after/plugin/nvim-lint.lua
@@ -7,4 +7,13 @@ lint.linters_by_ft = {
     json = {'jsonlint'},
     markdown = {'markdownlint-cli2'}
 
-  }
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+  end,
+})
