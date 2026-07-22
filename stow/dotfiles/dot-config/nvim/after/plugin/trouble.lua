982040f60fe3feb1d2aebf1b72f1ff55dc424919
Robin Björklin <robin.bjorklin@gmail.com>
2026-07-22 13:51:19 -0700

    fix(nvim): update activation command for Trouble plugin

--- stow/dotfiles/dot-config/nvim/after/plugin/trouble.lua
+++ stow/dotfiles/dot-config/nvim/after/plugin/trouble.lua
@@ -4,4 +4,4 @@ if (not status) then return end
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap("n", "<Leader>tt", "<Cmd>TroubleToggle<CR>", opts)
+keymap("n", "<Leader>tt", "<Cmd>Trouble<CR>", opts)
